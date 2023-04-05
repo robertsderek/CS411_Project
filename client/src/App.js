bc8a848910ad5e50b934c6957cd1b18f20fd340c
James <wong.james2003@gmail.com>
2023-04-04 23:28:09 -0400

    removed unnecessary files in client side

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,9 @@
-import React, { useState, useEffect } from "react";
-import "./App.css";
-
 function App() {
-  const [message, setMessage] = useState("");
-
-  useEffect(() => {
-    fetch("http://localhost:8000/message")
-      .then((res) => res.json())
-      .then((data) => setMessage(data.message));
-  }, []);
-
   return (
     <div className="App">
-      <h1>{message}</h1>
-      <button>Log In</button>
+      <h1>Welcome to Calendar</h1>
     </div>
-  );
+  )
 }
 
 export default App
