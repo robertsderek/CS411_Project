4d9b3192b23ab0af29b7bdd90edb530d80928b8e
James <wong.james2003@gmail.com>
2023-04-07 12:46:00 -0400

    created credentials format

--- server/weather.js
+++ server/weather.js
@@ -1,5 +1,6 @@
 //put key here
-let weatherAPI = 9999
+const { WEATHER_API_KEY } = require('./credentials.js')
+let key =  WEATHER_API_KEY
 
 async function getWeather(q){
     weatherCall = await fetch("http://api.weatherapi.com/v1/forecast.json?key="+key+"&q="+q+"&days=10&aqi=no&alerts=no")
