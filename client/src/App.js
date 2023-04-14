03bd50574859e472a999e99d8b861f73ee19eb16
robertsderek <derekr@bu.edu>
2023-04-14 12:42:53 -0400

    added front end map and current location function

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,11 @@
+import React from 'react';
+import Map from "./Map";
+
 function App() {
   return (
     <div className="App">
       <h1>Welcome to Calendar</h1>
+      <Map />
     </div>
   )
 }
