51a8017ba1bb64aaabe3eddee5e87d91a00aebf5
kwu71 <kwu7@bu.edu>
2023-04-18 20:59:52 -0400

    added verify to backend

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,44 @@
 import { GoogleLogin } from '@react-oauth/google';
+import { useState, useEffect } from 'react';
 import React from 'react';
 
+function Welcome({email}) {
+  return (
+    <h1>welcome {email}</h1>
+  )
+}
+
+function Ask(){
+  return (
+    <h1>LOG IN NOW!</h1>
+  )
+}
+
 function App() {
+  const [userEmail, setUserEmail] = useState("")
+  const [login, setLogin] = useState(false)
+
   return (
     <div className="App">
-      <GoogleLogin
+
+      {login 
+        ? <Welcome email={userEmail}/> 
+        : <GoogleLogin
         onSuccess={async (credentialResponse) => {
-          const response = await fetch('http://localhost:3001/oauth', {
+            fetch('http://localhost:3001/oauth', {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify(credentialResponse)
-          })
+            body: JSON.stringify(credentialResponse)})
+              .then(response => response.json())
+              .then(data => setUserEmail(data['email']))
+              .then(_ => setLogin(true))
+              .catch(error => console.log(error))
         }}
         onError={() => {
           console.log('Login Failed');
-        }}
-      />
+        }}/>
+      }
+      
     </div>
   )
 }
--- client/src/index.js
+++ client/src/index.js
@@ -5,8 +5,9 @@ import App from './App';
 import { GoogleOAuthProvider } from '@react-oauth/google';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
-  <GoogleOAuthProvider clientId="630236275912-is23ok94ogc5fb7bl1st4p5q0hegrd51.apps.googleusercontent.com">
+  <GoogleOAuthProvider clientId = "630236275912-is23ok94ogc5fb7bl1st4p5q0hegrd51.apps.googleusercontent.com">
     <React.StrictMode>
       <App />
     </React.StrictMode>
