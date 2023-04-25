7312d835d02e848631ee968a35d315d50bd61b6d
kwu71 <kwu7@bu.edu>
2023-04-25 11:51:48 -0400

    import oauth_credentials check out new file

--- client/src/index.js
+++ client/src/index.js
@@ -3,17 +3,17 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { GoogleOAuthProvider } from '@react-oauth/google';
+import {OAUTH_API} from './oauth_credentials'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-const { OAUTH_API } = require('./oauth_credentials.js')
-let key =  OAUTH_API
+console.log(OAUTH_API)
 
 root.render(
-  <GoogleOAuthProvider clientId = {key}>
+  <GoogleOAuthProvider clientId = {OAUTH_API}>
     <React.StrictMode>
       <App />
     </React.StrictMode>
-  //</GoogleOAuthProvider>,
+  </GoogleOAuthProvider>,
   document.getElementById('root')
 );
